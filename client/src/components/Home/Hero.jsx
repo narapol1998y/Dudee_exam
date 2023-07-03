5fc34ca97875d8557d262e166f5680d4abf84b83
narapol1998y <narapol25411998@gmail.com>
2023-07-03 13:04:57 +0700

    Test push

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Dudee Laundry</h1>
+                        <h1 className="mb-5 text-5xl font-bold">Test Test</h1>
                         <p className="mb-5">Welcome to Dudee Laundry , your premier destination for all your laundry needs. We are thrilled to introduce ourselves as the newest addition to the vibrant and bustling laundromat scene in Chiangmai.</p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
